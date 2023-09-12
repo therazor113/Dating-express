a989606d736b7af0ae29465cfbc2d5aac59a4f3e
therazor113 <therazor113@gmail.com>
2023-09-11 22:39:52 -0600

    Updated port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 4015
 
 app.use((_, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
